7df1e55bc074698640db4b1caf58118d94004578
ItsTimeToGetOut <43565190+ItsTimeToGetOut@users.noreply.github.com>
2019-10-23 02:42:23 +0530

    hacktoberfest

--- lib/src/swiper.dart
+++ lib/src/swiper.dart
@@ -18,8 +18,8 @@ const int kDefaultAutoplayDelayMs = 3000;
 ///  Default auto play transition duration (in millisecond)
 const int kDefaultAutoplayTransactionDuration = 300;
 
-const int kMaxValue = 2000000000;
-const int kMiddleValue = 1000000000;
+const int kMaxValue = 200000000;
+const int kMiddleValue = 100000000;
 
 enum SwiperLayout { DEFAULT, STACK, TINDER, CUSTOM }
 
