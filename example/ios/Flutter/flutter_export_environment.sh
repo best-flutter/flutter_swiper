707c8f468dab089feec835f48ea4cfeb45bc22f1
Faiyyazauxy <faiyyazk@auxesisinfotech.com>
2021-03-31 17:55:38 +0530

    handle null context in getsize

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/faiyyaz/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/faiyyaz/AndroidStudioProjects/flutter_swiper/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/home/faiyyaz/snap/flutter/common/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
